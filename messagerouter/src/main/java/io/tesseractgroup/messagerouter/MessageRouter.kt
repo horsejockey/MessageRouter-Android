fa857734213993003eaea401a886d5effc5e7f60
Matthew McArthur <matthew.s.mcarthur@gmail.com>
2018-02-28 14:25:48 -0700

    Message router

---
+++ messagerouter/src/main/java/io/tesseractgroup/messagerouter/MessageRouter.kt
@@ -0,0 +1,156 @@
+package io.tesseractgroup.messagerouter
+
+import java.lang.ref.WeakReference
+
+/**
+ * MessageRouterApp
+ * Created by matt on 2/22/18.
+ */
+typealias Recipient = Any
+
+@Suppress("UNCHECKED_CAST")
+/**
+A class for sending messages of type T to the registered recipients. Can be a
+replacement to many types of delegate callbacks.
+ */
+class MessageRouter<T> {
+
+    private var entries = listOf< MessageRouterEntry<T, Recipient> >()
+    private val lock = java.lang.Object()
+
+    /**
+    Convenience function for add(_:_:). Simply takes a function that will
+    receive all messages for the life time of this instance, or until the
+    returned entry is removed.
+
+    - parameter function: The function to receive any messages.
+    - returns: An opaque object that can be used to stop any further messages.
+     */
+    fun add(function: (T)->Unit) : MessageRouterEntry<T, Recipient> {
+        return add(this as Recipient){
+            function
+        }
+    }
+
+    /**
+    The given function will receive any messages for the life time of `object`.
+    Typically called like this:
+
+    recipients.add(self, self.dynamicType.handleMessage)
+
+    - parameter object: The object that owns the given function.
+    - parameter function: The function that will be called with any messages. Typically a function on `object`.
+    - returns: An opaque object that can be used to stop any further messages.
+     */
+    fun <R: Recipient> add(recipient: R, function: (R)->((T)->Unit)) : MessageRouterEntry<T, Recipient> {
+        val entry = MessageRouterEntry<T, R>(recipient, function) as MessageRouterEntry<T, Recipient>
+        synchronized(lock){
+            entries = entries.filter { it.getRecipient() != null }
+            entries += entry
+        }
+        return entry
+    }
+
+    /**
+    The given function will receive any messages for the life time of `object`.
+    Ensures that only one callback will be saved for the provided `object` instance.
+    Typically called like this:
+
+    recipients.add(self, self.dynamicType.handleMessage)
+
+    - parameter object: The object that owns the given function.
+    - parameter function: The function that will be called with any messages. Typically a function on `object`.
+    - returns: An opaque object that can be used to stop any further messages.
+     */
+    fun <R: Recipient> addOnce(recipient: R, function: (R)->((T)->Unit)) : MessageRouterEntry<T, Recipient> {
+        val entry = MessageRouterEntry<T, R>(recipient, function) as MessageRouterEntry<T, Recipient>
+        synchronized(lock){
+            entries = entries.filter { it.getRecipient() != null && it.getRecipient() != entry.getRecipient() }
+            entries += entry
+        }
+        return entry
+    }
+
+    /**
+    Removes the given entry from the list of recipients.
+
+    - parameter entry: The entry to remove.
+     */
+    fun remove(entry: MessageRouterEntry<T, Recipient>){
+        synchronized(lock){
+            entries = entries.filter { it.getRecipient() != null && it !== entry }
+        }
+    }
+
+    /**
+    Removes all entries for the provided recipient.
+
+    - parameter recipient: The recipient to remove.
+     */
+    fun remove(recipient: Recipient){
+        synchronized(lock){
+            entries = entries.filter { it.getRecipient() != null && it.getRecipient() !== recipient }
+        }
+    }
+
+    /**
+    Sends the given message to all the registered recipients.
+
+    - parameter message: The message to send to the recipients.
+     */
+    fun send(message: T){
+        var handlers = listOf<(T)->Unit>()
+
+        synchronized(lock){
+            val newEntries = mutableListOf<MessageRouterEntry<T, Recipient>>()
+
+            for(entry in entries){
+                val recipient = entry.getRecipient()
+                if (recipient != null){
+                    handlers += entry.function(recipient)
+                    newEntries.add(entry)
+                }
+            }
+
+            entries = newEntries
+        }
+
+        for(handler in handlers){
+            handler(message)
+        }
+    }
+
+
+    /**
+    Convenience method for getting a copy of the entries. This is intended only
+    for testing. Since `entries` is private.
+
+    - returns: A copy of the registered recipient entries.
+     */
+    fun copyEntries() : List< MessageRouterEntry<T, Recipient> > {
+        var copiedEntries = listOf< MessageRouterEntry<T, Recipient> >()
+
+        synchronized(lock) {
+            for (entry in entries) {
+                copiedEntries += entry
+            }
+        }
+
+        return entries
+    }
+
+}
+
+/// Opaque object for tracking message recipient info.
+class MessageRouterEntry<T, in R :Recipient>(recipient: R, val function: (R) -> ((T) -> Unit)) {
+
+    private var recipient: WeakReference<R>? = null
+
+    init {
+        this.recipient = WeakReference(recipient)
+    }
+
+    fun getRecipient() : Any? {
+        return recipient?.get()
+    }
+}
---
+++ messagerouter/src/test/java/io/tesseractgroup/messagerouter/MessageRouterTests.kt
@@ -0,0 +1,187 @@
+package io.tesseractgroup.messagerouter
+
+import org.amshove.kluent.shouldEqual
+import org.junit.Assert
+import org.junit.Test
+import java.lang.ref.WeakReference
+import java.lang.reflect.InvocationTargetException
+
+/**
+ * MessageRouterApp
+ * Created by matt on 2/22/18.
+ */
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class MessageRouterTests {
+
+    var router = MessageRouter<Int>()
+
+    @Test
+    fun testAddFunction() {
+        router.copyEntries().count() shouldEqual 0
+        router.add { }
+        router.copyEntries().count() shouldEqual 1
+    }
+
+    @Test
+    fun testAddRecipient() {
+        router.copyEntries().count() shouldEqual 0
+        val recipient = MessageRouterTestHelper()
+        router.add(recipient) { recipient.doNothing }
+        router.copyEntries().count() shouldEqual 1
+    }
+
+    @Test
+    fun testRemoveFunction() {
+        router.copyEntries().count() shouldEqual 0
+        val entry = router.add { }
+        router.copyEntries().count() shouldEqual 1
+        router.remove(entry)
+        router.copyEntries().count() shouldEqual 0
+    }
+
+    @Test
+    fun testRemoveRecipient() {
+        router.copyEntries().count() shouldEqual 0
+        val recipient = MessageRouterTestHelper()
+        val entry = router.add(recipient) { recipient.doNothing }
+        router.copyEntries().count() shouldEqual 1
+        router.remove(entry)
+        router.copyEntries().count() shouldEqual 0
+    }
+
+    @Test
+    fun testSend() {
+        // Sends a varying number of messages to a varying number of recipients.
+        for (recipientCount in 0..3) {
+            for (messageCount in 0..3) {
+                send(recipientCount, messageCount)
+            }
+        }
+    }
+
+    @Test
+    fun testSendAfterRemove() {
+        val entry = router.add { Assert.fail("Should not be called") }
+        router.remove(entry)
+        router.send(42)
+    }
+
+    data class TupleMessage(val string: String, val int: Int)
+
+    @Test
+    fun testSendTuple() {
+        var count = 0
+        val tupleRouter = MessageRouter<TupleMessage>()
+        tupleRouter.add { count += 1 }
+        tupleRouter.send(TupleMessage("Hello", 42))
+        count shouldEqual 1
+    }
+
+    @Test
+    fun testMemoryLeak() {
+        // Creates a recipient in this scope.
+        var recipient: MessageRouterTestHelper? = MessageRouterTestHelper()
+
+        router.add(recipient!!, recipient!!.test)
+        val verifier = MemoryLeakVerifier(recipient)
+        recipient = null
+        verifier.assertGarbageCollected("Should b gone")
+        router.send(42)
+    }
+
+    // MARK: - Helpers
+
+    private fun send(recipientCount: Int, messageCount: Int) {
+        var count = 0
+        val message = 42
+
+        // Adds n recipients.
+        if (recipientCount > 0) {
+            for (i in 1..recipientCount) {
+                router.add { n ->
+                    count += 1;
+                    n shouldEqual message
+                }
+            }
+        }
+
+        // Sends m messages to n recipients.
+        if (messageCount > 0) {
+            for (i in 1..messageCount) {
+                router.send(message)
+            }
+        }
+        count shouldEqual recipientCount * messageCount
+    }
+}
+
+private class MessageRouterTestHelper {
+
+    var hello = "hello"
+
+    val test = { r: MessageRouterTestHelper ->{ int: Int ->
+        hello = "Goodbye"
+        Assert.fail("Should not be called.")
+    } }
+
+    val fail = { int: Int ->
+        hello = "Goodbye"
+        Assert.fail("Should not be called.")
+    }
+
+    val doNothing = { int: Int ->
+        hello = "goodbye"
+    }
+}
+
+class MemoryLeakVerifier(testObject: Any) {
+
+    private val reference: WeakReference<Any>
+
+    val testObject: Any?
+        get() = reference.get()
+
+    init {
+        this.reference = WeakReference(testObject)
+    }
+
+    /**
+     * Attempts to perform a full garbage collection so that all weak references will be removed. Usually only
+     * a single GC is required, but there have been situations where some unused memory is not cleared up on the
+     * first pass. This method performs a full garbage collection and then validates that the weak reference
+     * now has been cleared. If it hasn't then the thread will sleep for 50 milliseconds and then retry up to
+     * 10 more times. If after this the object still has not been collected then the assertion will fail.
+     *
+     *
+     * Based upon the method described in: http://www.javaworld.com/javaworld/javatips/jw-javatip130.html
+     */
+    fun assertGarbageCollected(name: String) {
+        val runtime = Runtime.getRuntime()
+        for (i in 0 until MAX_GC_ITERATIONS) {
+            runtime.runFinalization()
+            runtime.gc()
+            if (testObject == null)
+                break
+
+            // Pause for a while and then go back around the loop to try again...
+            try {
+                Thread.sleep(GC_SLEEP_TIME.toLong())
+            } catch (e: InterruptedException) {
+                // Ignore any interrupts and just try again...
+            } catch (e: InvocationTargetException) {
+                // Ignore any interrupts and just try again...
+            }
+
+        }
+        testObject shouldEqual null
+    }
+
+    companion object {
+        private val MAX_GC_ITERATIONS = 50
+        private val GC_SLEEP_TIME = 100
+    }
+}
