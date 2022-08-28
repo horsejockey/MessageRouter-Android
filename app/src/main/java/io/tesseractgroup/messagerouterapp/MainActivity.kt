26c21e7e6fdcd25a18b48c2ce65367cf2de68491
Matthew McArthur <matthew.s.mcarthur@gmail.com>
2022-08-28 17:03:25 -0600

    target sdk 31

--- app/src/main/java/io/tesseractgroup/messagerouterapp/MainActivity.kt
+++ app/src/main/java/io/tesseractgroup/messagerouterapp/MainActivity.kt
@@ -1,7 +1,7 @@
 package io.tesseractgroup.messagerouterapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
